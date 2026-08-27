dd7317d554ceb371591499610e7e7246016c77e7
A-Words <awordsgg@outlook.com>
2026-08-27 17:56:00 +0800

    build(android): target Android 17

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "app.threadmind"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "0.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
